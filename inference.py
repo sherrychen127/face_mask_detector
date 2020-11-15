819918471918e6d699ae5cd131b1bc83ce0d0092
sherrychen127 <sherrychen127@gmail.com>
2020-11-14 20:29:27 -0500

    added train.py

--- inference.py
+++ inference.py
@@ -15,28 +15,26 @@ from detectron2.config import get_cfg
 from detectron2.utils.visualizer import Visualizer
 from detectron2.data import MetadataCatalog, DatasetCatalog
 import matplotlib.pyplot as plt
-
-
 from detectron2.engine import DefaultTrainer
+from detectron2.utils.visualizer import ColorMode
 
-# %load_ext tensorboard
-# %tensorboard --logdir output
+from facemask_dataset import register_facemask_dataset, get_facemask_1_dicts
 
-from dataset import get_balloon_dicts
 os.environ['CUDA_VISIBLE_DEVICES'] = '2'
 
 
 cfg = get_cfg()
-cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
-cfg.DATASETS.TRAIN = ("balloon_train",)
+cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_50_FPN_1x.yaml"))
+cfg.DATASETS.TRAIN = ("facemask_1_train",)
 cfg.DATASETS.TEST = ()
 cfg.DATALOADER.NUM_WORKERS = 2
-cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")  # Let training initialize from model zoo
+cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Detection/faster_rcnn_R_50_FPN_1x.yaml")  # Let training initialize from model zoo
 cfg.SOLVER.IMS_PER_BATCH = 2
 cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
 cfg.SOLVER.MAX_ITER = 300    # 300 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
 cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128   # faster, and good enough for this toy dataset (default: 512)
 cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # only has one class (ballon). (see https://detectron2.readthedocs.io/tutorials/datasets.html#update-the-config-for-new-datasets)
+cfg.OUTPUT_DIR = './output/facemask_train' #EDIT
 
 
 # Inference should use the config with parameters that are used in training
@@ -46,26 +44,41 @@ cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7   # set a custom testing threshold
 predictor = DefaultPredictor(cfg)
 
 
-from detectron2.utils.visualizer import ColorMode
+
+# print(predictor.has('pred_mask'))
+
+facemask_1_metadata = MetadataCatalog.get("facemask_1_train")
+
+# im = cv2.imread('1200.jpg')
+# outputs = predictor(im)  # format is documented at https://detectron2.readthedocs.io/tutorials/models.html#model-output-format
+# print(outputs)
+# print(outputs['instances'].has('pred_masks'))
+# v = Visualizer(im[:, :, ::-1],
+#                 metadata=facemask_1_metadata, 
+#                 scale=0.5, 
+#                 instance_mode=ColorMode.IMAGE_BW   # remove the colors of unsegmented pixels. This option is only available for segmentation models
+# )
+# out = v.draw_instance_predictions(outputs["instances"])#.to("cpu"))
+# # cv2_imshow(out.get_image()[:, :, ::-1])
+# print(out)
+# plt.imshow(out.get_image()[:, :, ::-1])
 
 
-# for d in ["train", "val"]:
-#     DatasetCatalog.register("balloon_" + d, lambda d=d: get_balloon_dicts("/home/sherry/APS360/datasets/balloon/" + d))
-#     MetadataCatalog.get("balloon_" + d).set(thing_classes=["balloon"])
-balloon_metadata = MetadataCatalog.get("balloon_train")
-
-dataset_dicts = get_balloon_dicts("/home/sherry/APS360/datasets/balloon/val")
+facemask_1_metadata = MetadataCatalog.get("facemask_1_train")
+# dataset_dicts = DatasetCatalog.get("facemask_1_train")
+dataset_dicts = get_facemask_1_dicts("/home/sherry/APS360/datasets/face_mask_dataset/Medical mask/Medical mask/Medical Mask")
 
 for d in random.sample(dataset_dicts, 3):    
     im = cv2.imread(d["file_name"])
     outputs = predictor(im)  # format is documented at https://detectron2.readthedocs.io/tutorials/models.html#model-output-format
-    v = Visualizer(im[:, :, ::-1],
-                   metadata=balloon_metadata, 
-                   scale=0.5, 
-                   instance_mode=ColorMode.IMAGE_BW   # remove the colors of unsegmented pixels. This option is only available for segmentation models
-    )
-    out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
-    # cv2_imshow(out.get_image()[:, :, ::-1])
-    print(out)
-    plt.imshow(out.get_image()[:, :, ::-1])
+    print(outputs)
+    # v = Visualizer(im[:, :, ::-1],
+    #                metadata=facemask_1_metadata, 
+    #                scale=0.5, 
+    #                instance_mode=ColorMode.IMAGE_BW   # remove the colors of unsegmented pixels. This option is only available for segmentation models
+    # )
+    # out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+    # # cv2_imshow(out.get_image()[:, :, ::-1])
+    # print(out)
+    # plt.imshow(out.get_image()[:, :, ::-1])
 plt.show()
---
+++ train.py
@@ -0,0 +1,47 @@
+# Some basic setup:
+# Setup detectron2 logger
+import detectron2
+from detectron2.utils.logger import setup_logger
+setup_logger()
+
+# import some common libraries
+import numpy as np
+import os, json, cv2, random
+
+# import some common detectron2 utilities
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.engine import DefaultTrainer
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog, DatasetCatalog
+import matplotlib.pyplot as plt
+
+from facemask_dataset import register_facemask_dataset, get_facemask_1_dicts
+
+
+os.environ['CUDA_VISIBLE_DEVICES'] = '2'
+
+facemask_1_metadata, dataset_dicts = register_facemask_dataset()
+
+# facemask_1_metadata = MetadataCatalog.get("facemask_1_train")
+# dataset_dicts = get_facemask_1_dicts("/home/sherry/APS360/datasets/face_mask_dataset/Medical mask/Medical mask/Medical Mask")
+
+
+cfg = get_cfg()
+cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_50_FPN_1x.yaml"))
+cfg.DATASETS.TRAIN = ("facemask_1_train",)
+cfg.DATASETS.TEST = ()
+cfg.DATALOADER.NUM_WORKERS = 2
+cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Detection/faster_rcnn_R_50_FPN_1x.yaml")  # Let training initialize from model zoo
+cfg.SOLVER.IMS_PER_BATCH = 6 #2
+cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
+cfg.SOLVER.MAX_ITER = 2000    # 300 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
+cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128   # faster, and good enough for this toy dataset (default: 512)
+cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # only has one class (ballon). (see https://detectron2.readthedocs.io/tutorials/datasets.html#update-the-config-for-new-datasets)
+cfg.OUTPUT_DIR = './output/facemask_train'
+
+os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
+trainer = DefaultTrainer(cfg) 
+trainer.resume_or_load(resume=True)
+trainer.train()
